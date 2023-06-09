2c5f56b38b783937b6d54f548f890106a4763295
Justin <justinschwerdtfeger245@gmail.com>
2023-06-09 17:45:03 -0500

    created basic project (on the correct branch)

---
+++ ftc/Motor.java
@@ -0,0 +1,17 @@
+package ftc;
+
+public class Motor {
+    private final int portNumber;
+
+    public Motor(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    public int getPortNumber() {
+        return portNumber;
+    }
+
+    public void setPower(double power) {
+        
+    }
+}
